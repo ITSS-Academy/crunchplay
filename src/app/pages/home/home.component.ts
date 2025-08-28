de35943fef6eb053ad1f98d666006cc02ed16435
Dung <anhbietroi2000@gmail.com>
2025-08-28 10:46:34 +0700

    feat: enhance home, liked videos, playlist, and video components with new layouts and data binding

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,15 +1,24 @@
 import {Component} from '@angular/core';
-import {MatButton} from '@angular/material/button';
+import {MatButton, MatButtonModule} from '@angular/material/button';
 import supabase from '../../utils/supabase';
 import {HomeCardComponent} from '../../components/home-card/home-card.component';
 import {AuthService} from '../../services/auth/auth.service';
+import {CommonModule} from '@angular/common';
+import {MatCardModule} from '@angular/material/card';
+import {MatIconModule} from '@angular/material/icon';
+
+interface Video {
+  id: number;
+  title: string;
+  thumbnail: string;
+  channel: string;
+  views: string;
+  time: string;
+}
 
 @Component({
   selector: 'app-home',
-  imports: [
-    MatButton,
-    HomeCardComponent
-  ],
+  imports: [CommonModule, MatCardModule, MatIconModule, MatButtonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
@@ -17,21 +26,110 @@ export class HomeComponent {
 
   constructor(private authService: AuthService) {
   }
-
-  // async login() {
-  //   await this.authService.login();
-  // }
-  //
-  // async printIdToken() {
-  //   const {data, error} = await supabase.auth.getSession();
-  //   if (error) {
-  //     console.error('Error fetching session:', error);
-  //     return;
-  //   }
-  //   if (data.session) {
-  //     console.log('ID Token:', data.session.access_token);
-  //   } else {
-  //     console.log('No active session found.');
-  //   }
-  // }
+  videos: Video[] = [
+    {
+      id: 1,
+      title: 'Học code web',
+      thumbnail: 'https://cellphones.com.vn/sforum/wp-content/uploads/2022/09/43.jpg',
+      channel: 'Angular Mastery',
+      views: '15K',
+      time: '2 ngày trước'
+    },
+    {
+      id: 2,
+      title: 'Múa ',
+      thumbnail: 'https://cellphones.com.vn/sforum/wp-content/uploads/2022/09/48.jpg',
+      channel: 'Code Dạo',
+      views: '32K',
+      time: '1 tuần trước'
+    },
+    {
+      id: 3,
+      title: 'Faptv nè con',
+      thumbnail: 'https://cellphones.com.vn/sforum/wp-content/uploads/2022/09/50.jpg',
+      channel: 'TechMaster',
+      views: '9K',
+      time: '3 tuần trước'
+    },
+    {
+      id: 4,
+      title: 'Chupapi',
+      thumbnail: 'https://cellphones.com.vn/sforum/wp-content/uploads/2022/09/15.jpg',
+      channel: 'TechMaster',
+      views: '9K',
+      time: '3 tuần trước'
+    },
+    {
+      id: 5,
+      title: 'where are you now ',
+      thumbnail: 'https://cellphones.com.vn/sforum/wp-content/uploads/2022/09/23.jpg',
+      channel: 'TechMaster',
+      views: '9K',
+      time: '3 tuần trước'
+    },
+    {
+      id: 6,
+      title: 'Tiger',
+      thumbnail: 'https://cellphones.com.vn/sforum/wp-content/uploads/2022/09/25.jpg',
+      channel: 'TechMaster',
+      views: '9K',
+      time: '3 tuần trước'
+    },
+    {
+      id: 7,
+      title: 'chilllbro',
+      thumbnail: 'https://cellphones.com.vn/sforum/wp-content/uploads/2022/09/26.jpg',
+      channel: 'TechMaster',
+      views: '9K',
+      time: '3 tuần trước'
+    },
+    {
+      id: 8,
+      title: 'RxJS trong Angular dễ hiểu',
+      thumbnail: 'https://cellphones.com.vn/sforum/wp-content/uploads/2022/09/28.jpg',
+      channel: 'TechMaster',
+      views: '9K',
+      time: '3 tuần trước'
+    },
+    {
+      id: 9,
+      title: 'RxJS trong Angular dễ hiểu',
+      thumbnail: 'https://cellphones.com.vn/sforum/wp-content/uploads/2022/09/32.jpg',
+      channel: 'TechMaster',
+      views: '9K',
+      time: '3 tuần trước'
+    },
+    {
+      id: 12,
+      title: 'RxJS trong Angular dễ hiểu',
+      thumbnail: 'https://cellphones.com.vn/sforum/wp-content/uploads/2022/09/34.jpg',
+      channel: 'TechMaster',
+      views: '9K',
+      time: '3 tuần trước'
+    },
+    {
+      id: 10,
+      title: 'RxJS trong Angular dễ hiểu',
+      thumbnail: 'https://cellphones.com.vn/sforum/wp-content/uploads/2022/09/36.jpg',
+      channel: 'TechMaster',
+      views: '9K',
+      time: '3 tuần trước'
+    },
+    {
+      id: 11,
+      title: 'RxJS trong Angular dễ hiểu',
+      thumbnail: 'https://cellphones.com.vn/sforum/wp-content/uploads/2022/09/38.jpg',
+      channel: 'TechMaster',
+      views: '9K',
+      time: '3 tuần trước'
+    },
+    {
+      id: 12,
+      title: 'RxJS trong Angular dễ hiểu',
+      thumbnail: 'https://cellphones.com.vn/sforum/wp-content/uploads/2022/09/40.jpg',
+      channel: 'TechMaster',
+      views: '9K',
+      time: '3 tuần trước'
+    },
+  ];
 }
